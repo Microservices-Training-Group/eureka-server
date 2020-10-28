c92f1713747b44742193d288192267d4b2686215
Rinzin Norbu <45090425+Rinzin2018@users.noreply.github.com>
2020-10-28 17:50:08 +0600

    eureka server setup for API
    Eureka setup for microservices

---
+++ src/main/java/com/example/newseurekaserver/NewEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.newseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NewEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewEurekaServerApplication.class, args);
+	}
+
+}
